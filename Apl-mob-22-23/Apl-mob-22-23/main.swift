0f94dea759ebc23fccc836e85516ecb126d83426
lemonjuiceexe <czokofini@gmail.com>
2022-09-09 10:50:20 +0200

    09-09 Ex1 and part of ex2

--- Apl-mob-22-23/Apl-mob-22-23/main.swift
+++ Apl-mob-22-23/Apl-mob-22-23/main.swift
@@ -1,11 +1,43 @@
-//
-//  main.swift
-//  Apl-mob-22-23
-//
-//  Created by Franciszek Niwicki on 09/09/2022.
-//
+func ex1(){
+    let name = readLine()?.lowercased() ?? "";
+    if(name == ""){
+        print("Niepoprawne imie");
+        return;
+    }
+    
+    if(!(name[name.index(before: name.endIndex)] == "a")
+       || name == "kuba" || name == "barnaba" || name == "bonawentura"){
+        print("imie meskie");
+    }
+    else if(name == "maria"){
+        print("nie mozna stwierdzic jakie to imie")
+    }
+    else{
+        print("imie zenskie");
+    }
+}
 
-import Foundation
+func ex2(){
+    let p = readLine()?.lowercased() ?? "";
+    if(p == ""){
+        print("Podaj niepusty pesel");
+        return;
+    }
+    
+    //11 characters long
+    var correct = p.count == 11;
+    //Digits only
+    for c in p{
+        let temp = c.wholeNumberValue ?? -1;
+        if(temp == -1 || String(temp) != String(c)){
+            correct = false;
+            break;
+        }
+    }
+    //11th digit (control digit)
+    
+    
+    print(correct);
+}
 
-print("Hello, World!")
-
+ex2()
