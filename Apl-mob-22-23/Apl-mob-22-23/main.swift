1f32e812382e5ee18b7fa9093f9b11867e0f37b7
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2022-09-16 00:35:44 +0200

    Ex 9-9

--- Apl-mob-22-23/Apl-mob-22-23/main.swift
+++ Apl-mob-22-23/Apl-mob-22-23/main.swift
@@ -35,9 +35,79 @@ func ex2(){
         }
     }
     //11th digit (control digit)
+    var weight = [1, 3 ,7, 9, 1, 3, 7, 9, 1, 3, 1]
+    var sum = 0;
+    for i in 0...10{
+        print(sum)
+        print(String(p[p.index(p.startIndex, offsetBy: i)].wholeNumberValue ?? 0 * weight[i]) + " " + String(weight[i]))
+        sum += (p[p.index(p.startIndex, offsetBy: i)].wholeNumberValue ?? 0) * weight[i];
+    }
+    var temp = String(sum);
+    if((temp[temp.index(before: temp.endIndex)].wholeNumberValue ?? 0) != 0){
+        correct = false;
+    }
+    
+    if(!correct){
+        print("Pesel niepoprawny");
+        return;
+    }
+    print(p[p.index(p.startIndex, offsetBy: 9)].wholeNumberValue ?? 0);
+    if((p[p.index(p.startIndex, offsetBy: 9)].wholeNumberValue ?? 0) % 2 == 0){
+        print("Kobieta");
+    }
+    else{
+        print("Mezczyzna");
+    }
+    var century = "0";
+    if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 8
+    || (p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 9){
+        century = "18";
+    }
+    else if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 0
+    || (p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 1){
+        century = "19";
+    }
+    else if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 2
+    ||((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 3){
+        century = "20";
+    }
+    else if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 4
+    || (p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 5){
+        century = "21";
+    }
+    else if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 6
+    || (p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) == 7){
+        century = "22";
+    }
+    
+    var year = String(century) + String(p[p.index(p.startIndex, offsetBy: 0)].wholeNumberValue ?? 0) + String(p[p.index(p.startIndex, offsetBy: 1)].wholeNumberValue ?? 0);
+    var month = "0";
+    
+    if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) % 2 == 0
+    && p[p.index(p.startIndex, offsetBy: 3)].wholeNumberValue ?? 0 == 1){
+        month = "01";
+    }
+    else if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) % 2 != 0
+    && p[p.index(p.startIndex, offsetBy: 3)].wholeNumberValue ?? 0 == 1){
+        month = "11";
+    }
+    else if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) % 2 == 0
+    && p[p.index(p.startIndex, offsetBy: 3)].wholeNumberValue ?? 0 == 2){
+        month = "02";
+    }
+    else if((p[p.index(p.startIndex, offsetBy: 2)].wholeNumberValue ?? 0) % 2 != 0
+    && p[p.index(p.startIndex, offsetBy: 3)].wholeNumberValue ?? 0 == 2){
+        month = "12";
+    }
+    else{
+        month = String(p[p.index(p.startIndex, offsetBy: 3)].wholeNumberValue ?? 0) + String(p[p.index(p.startIndex, offsetBy: 5)].wholeNumberValue ?? 0);
+    }
+    
+    var day = String(p[p.index(p.startIndex, offsetBy: 4)].wholeNumberValue ?? 0) + String(p[p.index(p.startIndex, offsetBy: 5)].wholeNumberValue ?? 0);
     
     
-    print(correct);
+    //print("Urodzony/a " + day + "." + month + "." + year)
+    print(year + month + day);
 }
 
 ex2()
