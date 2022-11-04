56d8fe2d983dceca9ea3a34558d788601b365af4
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2022-11-04 10:52:42 +0100

    Add BMI project backend, assets, layout tweaks

--- BMI/BMI/ViewController1.swift
+++ BMI/BMI/ViewController1.swift
@@ -9,13 +9,32 @@ import UIKit
 
 class ViewController1: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var weightInput: UITextField!
     @IBOutlet weak var heightInput: UITextField!
-    
+    @IBOutlet weak var result: UILabel!
     //Calculate button onclick
     @IBAction func calculate(_ sender: Any) {
         if(weightInput.hasText && heightInput.hasText){
+            guard let weight = Double(weightInput.text ?? ""), let height = Double(heightInput.text ?? "") else{
+                result.text = ("Dane nie są poprawnymi liczbami!");
+                return;
+            }
+            let bmi = weight / ((height / 100) * (height / 100));
+            result.text = String(round(bmi * 10) / 10);
             
+            if(bmi >= 25){
+                image.image = UIImage(named: "big");
+            }
+            else if(bmi < 18.5){
+                image.image = UIImage(named: "small");
+            }
+            else{
+                image.image = UIImage(named: "medium");
+            }
+        }
+        else{
+            result.text = "Puste dane!";
         }
     }
     
