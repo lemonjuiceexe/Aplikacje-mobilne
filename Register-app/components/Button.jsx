f27c1d5f2b37fe6c0cce782605a834e64b6dff10
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2023-04-15 22:43:41 +0200

    Register app: make button component fully customisable

--- Register-app/components/Button.jsx
+++ Register-app/components/Button.jsx
@@ -3,14 +3,14 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 export default function Button(props) {
 	const styles = StyleSheet.create({
 		button: {
-			backgroundColor: props.backgroundColor,
-			paddingHorizontal: '12%',
-			paddingVertical: '2%',
-			borderRadius: 5,
-			margin: 10
+			backgroundColor: props.backgroundColor ? props.backgroundColor : 'cornflowerblue',
+			paddingHorizontal: props.paddingHorizontal ? props.paddingHorizontal : '11.5%',
+			paddingVertical: props.paddingVertical ? props.paddingVertical : '2%',
+			borderRadius: props.borderRadius ? props.borderRadius : 5,
+			margin: props.margin ? props.margin : 5,
 		},
 		buttonText: {
-			color: props.textColor,
+			color: props.textColor ? props.textColor : 'white',
 			fontSize: props.fontSize ? props.fontSize : 20,
 			textAlign: 'center',
 		}
