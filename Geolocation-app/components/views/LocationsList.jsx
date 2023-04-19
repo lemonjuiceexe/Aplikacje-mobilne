252a85d6548c3ca167ea691a4bab5f27a2688c3e
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2023-04-19 12:47:29 +0200

    Geolocation app: Initial commit, add main page with downloaded fonts

---
+++ Geolocation-app/components/views/LocationsList.jsx
@@ -0,0 +1,7 @@
+import {View, Text, ActivityIndicator} from "react-native";
+
+export default function LocationsList() {
+	return (
+		<Text>There will be list of locations in here, promise.</Text>
+	);
+}
---
+++ Geolocation-app/components/views/MainPage.jsx
@@ -0,0 +1,66 @@
+import {View, Text, ActivityIndicator, StyleSheet, TouchableOpacity} from "react-native";
+import * as Font from 'expo-font';
+
+import {useState, useEffect} from "react";
+
+
+export default function MainPage(){
+	const [fontsLoaded, setFontsLoaded] = useState(false);
+
+	useEffect(() => {
+		Font.loadAsync({
+			'dongle': require('../../assets/fonts/Dongle-Regular.ttf'),
+			'dongle-bold': require('../../assets/fonts/Dongle-Bold.ttf'),
+			'dongle-light': require('../../assets/fonts/Dongle-Light.ttf')
+		}).then(() => {
+			setFontsLoaded(true);
+		});
+	}, []);
+
+	if(!fontsLoaded){
+		return (
+			<View style={styles.container}>
+				<ActivityIndicator size="large" color="#0000ff" />
+			</View>
+		);
+	}
+	return (
+		<View style={styles.container}>
+			<View style={styles.headerContainer}>
+				<TouchableOpacity>
+					<Text style={styles.header}>Main Page</Text>
+				</TouchableOpacity>
+				<Text style={styles.text}>a pretty bottom text to look at</Text>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		height: '100%'
+	},
+	headerContainer: {
+		flex: 1,
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#3F51B5',
+	},
+	header: {
+		fontFamily: 'dongle-bold',
+		fontSize: 100,
+		textAlign: 'center',
+		color: "#FFFFFF",
+		marginTop: 50
+	},
+	text: {
+		textAlign: 'center',
+		fontFamily: 'dongle-light',
+		fontSize: 40,
+		color: "#FFFFFF",
+		marginTop: 50
+	}
+});
