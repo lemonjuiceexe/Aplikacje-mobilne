bd95ace1dbb5ff69b1a68ad31c07e7529ddb40e8
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2023-06-07 12:33:57 +0200

    Accelerometer app: add project sending data via websocket

---
+++ Accelerometer-app/server/server.js
@@ -0,0 +1,24 @@
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server(
+    { port: 8080 }, () => {
+        console.log("ws startuje na porcie " + 8080)
+    });
+
+//reakcja na podłączenie klienta i odesłanie komunikatu
+
+wss.on('connection', (ws, req) => {
+
+    //adres ip klienta
+
+    const clientip = req.connection.remoteAddress;
+
+    //reakcja na komunikat od klienta
+
+    ws.on('message', function message(data, isBinary) {
+        const message = isBinary ? data : data.toString();
+        console.log(message);
+      });
+
+});
